997ca56948bb5df756af685b1f4341e2fd53b5e0
Frank <frankkashere@gmail.com>
2025-04-20 11:53:13 +0300

    Initial commit

---
+++ ds_client/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+function App() {
+
+
+  return (
+    <div className="mt-8">
+      <h1 className='flex justify-center underline'>Welcome to my reast estate app!</h1>
+      <p className="flex justify-center">This is an app that helps one manage not only rent but tenants as well.</p>
+    </div>
+  )
+}
+
+export default App
