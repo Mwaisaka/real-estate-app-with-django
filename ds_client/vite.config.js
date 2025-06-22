8a422e76bb114170e44d5a9051961d936bbf0680
Frank <frankkashere@gmail.com>
2025-06-22 21:11:14 +0300

    Updated code

--- ds_client/vite.config.js
+++ ds_client/vite.config.js
@@ -3,4 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir:'../ds_server/static', // Customize build output directory
+     emptyOutDir: true,
+  },
+  root: './', // Vite project root (defaults to current directory)
 });
