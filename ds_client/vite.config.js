835d50834b369f7f577f35fed7523989a1fc5c6e
Frank <frankkashere@gmail.com>
2025-06-28 17:56:42 +0300

    Updated code 03

--- ds_client/vite.config.js
+++ ds_client/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: "/", // Ensures relative paths work correctly
   plugins: [react()],
   build: {
     outDir:'./build', // Customize build output directory
